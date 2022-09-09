5cc2825a3f5f97240236eae76adc23c3b8c4ef44
cavalarit <thiago.cavalari@gmail.com>
2022-09-09 09:54:35 +0100

    Add files via upload

---
+++ login/index.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var loginRoutes = require('./api/routes/loginRoutes');
+
+module.exports = async function (context, req) {
+    try {
+        loginRoutes(app);
+    } catch(error) {
+      const err = JSON.stringify(error);
+      context.res = {
+        status: 500,
+        body: `Request error. ${err}`
+      };
+    }
+  };
