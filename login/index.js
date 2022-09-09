8d91505b418d3f06917355633978c05399fbc91e
cavalarit <thiago.cavalari@gmail.com>
2022-09-09 11:10:30 +0100

    Update index.js

--- login/index.js
+++ login/index.js
@@ -1,15 +1,8 @@
-var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
 var loginRoutes = require('../api/routes/loginRoutes');
 
 module.exports = async function (context, req) {
     try {
-        loginRoutes(app);
+        loginRoutes(context, req);
     } catch(error) {
       const err = JSON.stringify(error);
       context.res = {
