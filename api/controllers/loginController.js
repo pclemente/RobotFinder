b02c9f3944bf6301731b490ff6e519c383ff1ff8
cavalarit <thiago.cavalari@gmail.com>
2022-09-09 10:51:58 +0100

    Update loginController.js

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -2,27 +2,30 @@
 
 exports.login = function(req, res) {
   if (USER_USER == req.body.user && USER_PASSWORD == req.body.password) {
-    // res.status(200);
-    // res.json({'token':'5DE6E0C53BB8BB6CBB392526FA278D47'});
+    res.status(200);
+    res.json({'token':'5DE6E0C53BB8BB6CBB392526FA278D47'});
     context.res = {
-      body: {'token':'5DE6E0C53BB8BB6CBB392526FA278D47'},
+      // body: {'token':'5DE6E0C53BB8BB6CBB392526FA278D47'},
+      body: JSON.stringify({'token':'5DE6E0C53BB8BB6CBB392526FA278D47'}),
       headers: {
           'Content-Type': 'application/json'
-    }
-  };
-    
+      }
+    };
+    console.log('5DE6E0C53BB8BB6CBB392526FA278D47');
     context.done();    
 
   }else{
-    // res.status(401);
-    // res.json({'error':'invalid user and password'});
+    res.status(401);
+    res.json({'error':'invalid user and password'});
     context.res = {
       status: 401,
-      body: {'error':'invalid user and password'},
+      body: JSON.stringify({'error':'invalid user and password'}),
+      // body: {'error':'invalid user and password'},
       headers: {
           'Content-Type': 'application/json'
-     }
-  };
+      }
+    };
+    console.log('invalid user and password');
     context.done();    
   }
 };
