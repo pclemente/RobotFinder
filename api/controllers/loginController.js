75d441dcef305422332486dededd3274b677f5f0
cavalarit <thiago.cavalari@gmail.com>
2022-09-09 10:39:16 +0100

    Update loginController.js

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -2,16 +2,27 @@
 
 exports.login = function(req, res) {
   if (USER_USER == req.body.user && USER_PASSWORD == req.body.password) {
-    res.status(200);
-    res.json({'token':'5DE6E0C53BB8BB6CBB392526FA278D47'});
-    context.res = res;
+    // res.status(200);
+    // res.json({'token':'5DE6E0C53BB8BB6CBB392526FA278D47'});
+    context.res = {
+      body: {'token':'5DE6E0C53BB8BB6CBB392526FA278D47'},
+      headers: {
+          'Content-Type': 'application/json'
+    }
+  };
     
     context.done();    
 
   }else{
-    res.status(401);
-    res.json({'error':'invalid user and password'});
-    context.res = res;
+    // res.status(401);
+    // res.json({'error':'invalid user and password'});
+    context.res = {
+      status: 401,
+      body: {'error':'invalid user and password'},
+      headers: {
+          'Content-Type': 'application/json'
+     }
+  };
     context.done();    
   }
 };
